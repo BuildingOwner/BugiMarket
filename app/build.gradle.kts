138d735eee45d5de16abcda9c0c50e8b10ce0ef2
BuildingOwner <wodhks0911@gmail.com>
2023-11-28 23:23:16 +0900

    판매글 목록 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,5 +50,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
 }
