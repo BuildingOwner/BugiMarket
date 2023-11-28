0aaeed3b24023d9568daf8980eb976e48a727837
BuildingOwner <wodhks0911@gmail.com>
2023-11-29 01:11:58 +0900

    실행 완료

--- app/src/main/java/com/example/bugimarket/MainActivity.kt
+++ app/src/main/java/com/example/bugimarket/MainActivity.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.example.bugimarket.auth.LoginActivity
-import com.example.chatproject.SendMessageActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -36,23 +35,6 @@ class MainActivity : AppCompatActivity() {
             startActivity(
                 Intent(this, HomeActivity::class.java)
             )
-            finish()
-        }
-
-        sendMessageButton.setOnClickListener {
-            // 판매 글 목록에서 판매자에게 메시지 보내기 버튼을 클릭하면 SendMessageActivity로 이동
-            val intent = Intent(this, SendMessageActivity::class.java)
-            startActivity(intent)
-        }
-
-        viewMessagesButton.setOnClickListener {
-            // 판매 글 목록에서 메시지 보기 버튼을 클릭하면 ViewMessageActivity로 이동
-            val intent = Intent(this, ViewMessageActivity::class.java)
-            startActivity(intent)
-        }
-
-        findViewById<Button>(R.id.button_signout)?.setOnClickListener {
-            Firebase.auth.signOut()
             finish()
         }
     }
