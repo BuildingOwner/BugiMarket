de180239517ee338705b0adea9cc05eff8271402
BuildingOwner <wodhks0911@gmail.com>
2023-11-30 00:08:40 +0900

    필터 추가

--- app/src/main/java/com/example/bugimarket/HomeActivity.kt
+++ app/src/main/java/com/example/bugimarket/HomeActivity.kt
@@ -11,6 +11,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,14 +21,24 @@ import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
 
-class ListItem(val title: String, val date: String, val price: String, val imageUrl: String, val userId: String, val documentId: String)
-class ListAdapter(val itemList: List<ListItem>) :
+class ListItem(
+    val title: String,
+    val date: String,
+    val price: String,
+    val imageUrl: String,
+    val userId: String,
+    val documentId: String,
+    val isSelling: Boolean
+)
+
+class ListAdapter(var itemList: List<ListItem>) :
     RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
     class MyViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val title: TextView = v.findViewById(R.id.listItemTitle)
         val date: TextView = v.findViewById(R.id.listItemDate)
         val price: TextView = v.findViewById(R.id.listItemPrice)
+        val isSellingText: TextView = v.findViewById(R.id.isSelling)
         val image: ImageView = v.findViewById(R.id.listItemImage)
     }
 
@@ -46,6 +57,13 @@ class ListAdapter(val itemList: List<ListItem>) :
         holder.title.text = itemList[position].title
         holder.date.text = itemList[position].date
         holder.price.text = itemList[position].price
+        val isSell = itemList[position].isSelling
+        if(isSell){
+            holder.isSellingText.text = "판매중"
+        }
+        else{
+            holder.isSellingText.text = "판매 완료"
+        }
 
         Glide.with(holder.image.context)
             .load(itemList[position].imageUrl)
@@ -61,8 +79,7 @@ class ListAdapter(val itemList: List<ListItem>) :
                     putExtra("documentId", itemList[position].documentId)
                 }
                 context.startActivity(intent)
-            }
-            else{
+            } else {
                 val intent = Intent(context, ProductViewActivity::class.java).apply {
                     putExtra("documentId", itemList[position].documentId)
                 }
@@ -77,6 +94,10 @@ class ListAdapter(val itemList: List<ListItem>) :
 class HomeActivity : AppCompatActivity() {
     private lateinit var adapter: ListAdapter
     private var itemList: ArrayList<ListItem> = ArrayList()
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var homeBtn: Button
+    // 드롭다운 메뉴에서 선택한 옵션을 저장할 변수
+    private var selectedOption: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
@@ -89,6 +110,10 @@ class HomeActivity : AppCompatActivity() {
         val dividerItemDecoration =
             DividerItemDecoration(recyclerView.context, LinearLayoutManager(this).orientation)
         recyclerView.addItemDecoration(dividerItemDecoration)
+
+        // 드롭다운 메뉴 초기화
+        homeBtn = findViewById<Button>(R.id.filterBtn)
+        initDropdownMenu()
 
         val db = FirebaseFirestore.getInstance()
 
@@ -114,7 +139,8 @@ class HomeActivity : AppCompatActivity() {
                         val imageUrl = imageUrlList.firstOrNull() ?: ""
                         val userId = document.getString("userId") ?: ""
                         val documentId = document.id
-                        addItemToList(title, date, price, imageUrl, userId, documentId)
+                        val isSelling = document.getBoolean("isSelling") ?: false
+                        addItemToList(title, date, price, imageUrl, userId, documentId, isSelling)
                     }
 
                     // 어댑터에 데이터 변경 알림
@@ -147,7 +173,59 @@ class HomeActivity : AppCompatActivity() {
     }
 
     // 아이템 추가 함수
-    private fun addItemToList(title: String, date: String, price: String, imageUrl: String, userId: String, documentId: String) {
-        itemList.add(ListItem(title, date, price, imageUrl, userId, documentId))
+    private fun addItemToList(
+        title: String,
+        date: String,
+        price: String,
+        imageUrl: String,
+        userId: String,
+        documentId: String,
+        isSelling: Boolean
+    ) {
+        itemList.add(ListItem(title, date, price, imageUrl, userId, documentId, isSelling))
+    }
+
+    private fun initDropdownMenu() {
+        val popupMenu = PopupMenu(this, homeBtn)
+        popupMenu.menuInflater.inflate(R.menu.dropdown_menu, popupMenu.menu)
+
+        // 드롭다운 메뉴에서 선택한 옵션을 처리하는 리스너
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_selling -> {
+                    selectedOption = "판매중"
+                    filterItems()
+                    true
+                }
+                R.id.menu_sold_out -> {
+                    selectedOption = "판매 완료"
+                    filterItems()
+                    true
+                }
+                R.id.menu_all -> {
+                    selectedOption = "모두 보기"
+                    filterItems()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // 드롭다운 메뉴 버튼 클릭 시 팝업 메뉴 표시
+        homeBtn.setOnClickListener {
+            popupMenu.show()
+        }
+    }
+
+    // itemList를 선택한 옵션에 따라 필터링하는 함수
+    private fun filterItems() {
+        val filteredList = when (selectedOption) {
+            "판매중" -> itemList.filter { it.isSelling }
+            "판매 완료" -> itemList.filter { !it.isSelling }
+            else -> itemList
+        }
+
+        adapter.itemList = filteredList
+        adapter.notifyDataSetChanged()
     }
 }
